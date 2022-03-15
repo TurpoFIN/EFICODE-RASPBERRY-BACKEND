30f4d7c021a92c6ddf72509befb5fa2a0ecd9408
TurpoFIN <turpofin@gmail.com>
2022-03-15 20:08:32 +0200

    Some code to start developing the IO device script

--- app/app.js
+++ app/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cFilter = require('../PRIVATE/MODULES/cFilter');
+const dHandler = require('../PRIVATE/MODULES/dataHandler');
+const dbCon = require('../PRIVATE/MODULES/mysql-handler');
 const fs = require('fs');
+const { env } = require('process');
 
 const app = express();
+const cFilterSession = new cFilter.filterSession(10, 1000, 3);
 const dir = __dirname.split('\\app')[0]
 
 require('dotenv').config();
+
+console.log(env.MYSQL_HOST);
 
 app.listen(process.env.PORT ? process.env.PORT : 80);
 
@@ -18,11 +24,21 @@ app.post('/', (req, res) => {
 });
 
 app.post('/API/IO_DEVICES/DATA/UPDATE', (req, res) => {
-    if (req.body.clientKey) {
+    if (cFilterSession.checkConnection({ip: req.ip})) {
+        if (req.body.clientKey) {
 
+        }
+    } else {
+        res.end();
     }
 });
 
 app.post('/API/IO_DEVICES/CONFIG/GET', (req, res) => {
-    
+    if (cFilterSession.checkConnection({ip: req.ip})) {
+        if (req.body.clientKey) {
+            
+        }
+    } else {
+        res.end();
+    }
 })
